0a6e63a44381101b452921c73f22c953b152d9bc
Mogkc <carson.mogk@cox.net>
2019-08-10 17:53:46 -0400

    remembers and displays current selected word

--- public/onButtonPress.js
+++ public/onButtonPress.js
@@ -1,25 +1,51 @@
 // The game is about creating a word from the letters provided.
 // After the user is happy with their word, they'll submit it.
-const word = []
+let word = []
 
-// This function is referenced by name in createBoard.js
-const getLocation = function() {
-    event.preventDefault();
-    const letter = event.target;
-    // Every letter is in a column which is in a row, 
-    // and each row/col has a val attribute with its index
-    const location = {
-        col: parseInt(letter.parentElement.getAttribute("val")),
-        row: parseInt(letter.parentElement.parentElement.getAttribute("val"))
-    };
-    // If they click on a letter they've already selected,
-    // Clear the selection up to that point.
-    if(word.indexOf(location) != -1) {
-        word = word.splice(0, word.indexOf(location));
-    } else { // Otherwise, add the letter they clicked on to the word
-        word.push(letter)
+class Letter {
+    constructor(row, col, content) {
+        this.row = row;
+        this.col = col;
+        this.content = content;
     }
-    console.log(letter.textContent);
-    console.log(location);
+
+    equals(obj) {
+        return obj.row === this.row && obj.col === this.col;
+    }
 }
 
+// This function is referenced by name in createBoard.js
+const getLocation = function () {
+    event.preventDefault();
+    // Every letter is in a column which is in a row, 
+    // and each row/col has a val attribute with its index
+    const letter = new Letter(
+        parseInt(event.target.parentElement.getAttribute("val")), //row
+        parseInt(event.target.parentElement.parentElement.getAttribute("val")), //col
+        event.target.textContent //content
+    );
+    // If they click on a letter they've already selected,
+    // Remove that point and any that follows from their word
+    let upto;
+    word.forEach((other, i) => {
+        if (letter.equals(other)) {
+            upto = i;
+        }
+    });
+    if (upto != undefined) {
+        word = word.splice(0, upto);
+    } else {
+        // Otherwise, add the letter they clicked on to the word
+        word.push(letter);
+    }
+    displayWord();
+}
+
+const displayWord = function () {
+    current = document.getElementById("current");
+    let disp = "";
+    word.forEach(letter => {
+        disp += letter.content;
+    });
+    current.textContent = disp;
+}
