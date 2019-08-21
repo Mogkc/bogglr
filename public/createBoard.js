51f08431f04ddd367d6b60a3fdb68601cd42d168
Mogkc <carson.mogk@cox.net>
2019-08-21 14:45:50 -0400

    game tiles colored according to interactability

--- public/createBoard.js
+++ public/createBoard.js
@@ -1,6 +1,7 @@
-const board = document.getElementById("game_board");
+const board_front = document.getElementById("game_board");
 // Don't let maxCol be greater than 12.
 let row = false, col = false, maxCol = 8;
+const board = [];
 
 // This is added to the beginning and end of rows to help with centering
 const addBufferColumn = function () {
@@ -13,17 +14,27 @@ const addRow = function () {
     if(row) addBufferColumn(); // To cap the previous row
     const next_row = document.createElement("div")
     next_row.setAttribute("class", "row pb-sm-1");
-    next_row.setAttribute("val", `${board.children.length}`);
-    board.append(next_row);
+    next_row.setAttribute("val", `${board_front.children.length}`);
+    board_front.append(next_row);
     row = next_row;
     // And start the new row with a buffer column
     addBufferColumn();
+    board.push([]);
 }
 
 const addColumn = function (element) {
     if (!row || row.children.length > maxCol) {
         addRow();
     }
+    // Take care of the virtual board
+    board[board.length-1].push(
+        new Letter(
+            board[board.length-1].length,
+            board.length,
+            element.textContent
+        )
+    );
+    // Then add it to the page
     const next_column = document.createElement("div");
     next_column.setAttribute("class", "col-md-1");
     next_column.setAttribute("val", `${row.children.length-1}`); // -1 to ignore buffer
@@ -32,16 +43,15 @@ const addColumn = function (element) {
     col = next_column;
 }
 
-const addGameTile = function (letter) {
+const addGameTile = function () {
     const tile = document.createElement("button");
     tile.setAttribute("class", "tile btn btn-info");
     // getLocation is a function in onButtonPress.js
     tile.setAttribute("onclick", "getLocation()")
-    tile.textContent = letter;
+    tile.textContent = String.fromCharCode(65 + Math.floor(Math.random() * 26));
     addColumn(tile);
 }
 
 for(let i = 0; i < 32; i++) {
-    let char = Math.floor(Math.random() * 26);
-    addGameTile(String.fromCharCode(65 + char));
+    addGameTile();
 }
--- public/onButtonPress.js
+++ public/onButtonPress.js
@@ -7,23 +7,16 @@ const getLocation = function () {
     event.preventDefault();
     // Every letter is in a column which is in a row, 
     // and each row/col has a val attribute with its index
-    const letter = new Letter(
-        parseInt(event.target.parentElement.getAttribute("val")), //col
-        parseInt(event.target.parentElement.parentElement.getAttribute("val")), //row
-        event.target.textContent //content
-    );
+    const letter = board[parseInt(event.target.parentElement.parentElement.getAttribute("val"))][parseInt(event.target.parentElement.getAttribute("val"))]
     // If they click on a letter they've already selected,
     // Remove that point and any that follows from their word
     if (!shortenWord(letter)) {
         // Otherwise, try to add the letter they clicked on to the word
         if (isValid(letter)) {
             word.push(letter);
-            // And show that the letter's been selected
-            event.target.setAttribute("class", "tile btn btn-success");
-        } else {
-            alert("Please select a letter closer to your previous choice");
         }
     }
+    updateBoardColors();
     displayWord();
 }
 
@@ -47,21 +40,30 @@ const shortenWord = function (cutoff) {
             shortening = true;
             newLength = i;
         }
-        if (shortening) {
-            // Col is one greater when grabbing from the dom because
-            // there's a centering buffer
-            const buttonHoldingLetter = document.getElementById("game_board")
-                .children[letter.row]
-                .children[letter.col + 1]
-                .children[0];
-            // Show that the button is no longer selected
-            buttonHoldingLetter.setAttribute("class", "tile btn btn-info");
-        }
     });
     if (shortening) {
         word = word.splice(0, newLength);
     }
     return shortening;
+}
+
+const updateBoardColors = function () {
+    board.forEach((row, rowIndex) => {
+        row.forEach((letter, columnIndex) => {
+            const elementHoldingLetter = document.getElementById("game_board")
+                .children[rowIndex]
+                .children[columnIndex + 1] // +1 for buffer column
+                .children[0];
+            const inWord = word.reduce((acc, curr) => acc || curr.equals(letter), false);
+            if (inWord) { // Show player what's in the word
+                elementHoldingLetter.setAttribute("class", "tile btn btn-success");
+            } else if (isValid(letter)) { // Show player what they can add to the word
+                elementHoldingLetter.setAttribute("class", "tile btn btn-info");
+            } else { // Show that these tiles can't be interacted with
+                elementHoldingLetter.setAttribute("class", "tile btn btn-light");
+            }
+        })
+    })
 }
 
 const displayWord = function () {
