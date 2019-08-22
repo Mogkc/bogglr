5fc212ff634f569ab76475f0190882f49d03cd14
Mogkc <carson.mogk@cox.net>
2019-08-22 17:28:29 -0400

    refactored sendToServer for clarity

--- public/submit.js
+++ public/submit.js
@@ -2,28 +2,8 @@ const minWordLength = 1;
 
 const submit = function () {
     // Submit the word
-    if(word.length < minWordLength) return;
-    sendToServer(word, (word, veracity) => {
-        const text = word.reduce((acc, curr) => acc + curr.content, "");
-        const result = document.createElement("li");
-        if (!veracity) {
-            result.textContent = `Sorry, ${text} isn't in our dictionary`;
-        } else {
-            result.textContent = `Good Job! Click to see the definition: `;
-            const linkToDictionary = document.createElement("a");
-            linkToDictionary.setAttribute("href", `https://www.dictionary.com/browse/${text}`);
-            linkToDictionary.setAttribute("target", "_blank");
-            linkToDictionary.textContent = text;
-            result.append(linkToDictionary);
-            // Then add a 'remove from list' button
-            const remove = document.createElement("button");
-            remove.setAttribute("class", "btn btn-danger");
-            remove.setAttribute("onclick", "removeResult()");
-            remove.textContent = "X";
-            result.append(remove);
-        }
-        document.getElementById("results").append(result);
-    });
+    if (word.length < minWordLength) return;
+    sendToServer(word, displayResult);
     // Then clear it and let the player start making another
     word = [];
     updateBoardColors();
@@ -33,7 +13,29 @@ const sendToServer = function (word, callback) {
     // When there's a backend, this will check he word's veracity
     const veracity = true; // Hard coded for now
     // After recieving a response from the server, it will complete the callback.
-    callback(word, veracity)
+    callback(word, veracity);
+}
+
+const displayResult = (word, veracity) => {
+    const text = word.reduce((acc, curr) => acc + curr.content, "");
+    const result = document.createElement("li");
+    if (!veracity) {
+        result.textContent = `Sorry, ${text} isn't in our dictionary`;
+    } else {
+        result.textContent = `Good Job! Click to see the definition: `;
+        const linkToDictionary = document.createElement("a");
+        linkToDictionary.setAttribute("href", `https://www.dictionary.com/browse/${text}`);
+        linkToDictionary.setAttribute("target", "_blank");
+        linkToDictionary.textContent = text;
+        result.append(linkToDictionary);
+        // Then add a 'remove from list' button
+        const remove = document.createElement("button");
+        remove.setAttribute("class", "btn btn-danger");
+        remove.setAttribute("onclick", "removeResult()");
+        remove.textContent = "X";
+        result.append(remove);
+    }
+    document.getElementById("results").append(result);
 }
 
 const removeResult = function () {
